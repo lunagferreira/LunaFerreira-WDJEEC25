9558d506380105a25d14c00838f5e4291e72f635
lunagferreira <luna.ferreira@tecnico.ulisboa.pt>
2024-04-29 22:26:57 +0100

    Structure

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+  <div id="app">
+  <CarouselComponent/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import CarouselComponent from './components/CarouselComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    CarouselComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CarouselComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <Carousel>
+    <Slide v-for="(content, index) in slideContents" :key="index">
+      <div class="carousel__item">{{ content }}</div>
+    </Slide>
+    <template #addons>
+      <Navigation />
+    </template>
+  </Carousel>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { Carousel, Slide } from 'vue3-carousel';
+import 'vue3-carousel/dist/carousel.css';
+
+export default defineComponent({
+  name: 'CarouselComponent',
+  components: {
+    Carousel,
+    Slide
+  },
+  data() {
+    return {
+      slideContents: [
+        '1', // Content for the 1st slide
+        '2', 
+        '3', 
+        '4'  
+      ]
+    };
+  },
+})
+</script>
+
+<style scoped>
+.carousel__item {
+  height: 90vh; /* 90% of the viewport height */
+  width: 98%;
+  background-color: #376099;
+  color: white; /* Text color */
+  font-size: 40px;
+  border-radius: 20px; /* Rounded corners */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
