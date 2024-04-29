21999aba89260807fd97e14c17dc98dc0ff302dc
Luna <144795556+lunagferreira@users.noreply.github.com>
2024-04-29 23:24:41 +0100

    First slide done

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <Carousel>
-    <Slide v-for="slide in 4" :key="slide">
-      <div class="carousel__item">JEEC</div>
-    </Slide>
-  </Carousel>
+  <div>
+    <Carousel v-model="slideIndex">
+      <Slide v-for="(content, index) in slideContents" :key="index">
+        <div class="carousel__item">
+          <div v-if="typeof content === 'object' && content.question">
+            {{ content.question }}
+          </div>
+          <div v-else>
+            {{ content }}
+          </div>
+        </div>
+      </Slide>
+      <template #addons>
+        <Navigation />
+      </template>
+    </Carousel>
+    <!-- Answer outside the carousel -->
+    <div class="slide-answer" v-if="slideContents[slideIndex].answer">
+      Answer: {{ slideContents[slideIndex].answer }}
+    </div>
+  </div>
 </template>
+
 
 <script>
 import { defineComponent } from 'vue';
@@ -17,14 +34,25 @@ export default defineComponent({
     Carousel,
     Slide
   },
+    data() {
+    return {
+        slideIndex: 0,
+        slideContents: [
+        { question: "What is the capital of Portugal?", answer: "Lisbon" },
+        'Enter a date:', 
+        'Slide 3', 
+        'Slide 4'
+        ]
+    };
+    },
 })
 </script>
 
 <style scoped>
 .carousel__item {
-  height: 90vh; /* 90% of the viewport height */
-  width: 90%;
-  background-color: #955fa1;
+  height: 80vh; /* 80% of the viewport height */
+  width: 95%;
+  background-color: #376099;
   color: white; /* Text color */
   font-size: 40px;
   border-radius: 20px; /* Rounded corners */
@@ -32,4 +60,12 @@ export default defineComponent({
   justify-content: center;
   align-items: center;
 }
+  
+.slide-answer {
+  margin-top: 20px;
+  font-size: 32px;
+  color: #c1701a; 
+  text-align: center;
+}
 </style>
+
